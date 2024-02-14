dd841347def34f5ce004674a4c815fb4b2007415
evelynmitchell <efmphone@gmail.com>
2024-02-13 19:32:56 -0700

    typo

--- tsmixer/data_load.py
+++ tsmixer/data_load.py
@@ -7,7 +7,7 @@ DatasetDict({
     train_test: Dataset({
         features: ['start', 'target', 'item_id', 'feat_static_cat', 'feat_static_real', 'past_feat_dynamic_real'],
         num_rows: 17568
-    })
+    }),
     pretrain: Dataset({
         features: ['start', 'target', 'item_id', 'feat_static_cat', 'feat_static_real', 'past_feat_dynamic_real'],
         num_rows: 159472
