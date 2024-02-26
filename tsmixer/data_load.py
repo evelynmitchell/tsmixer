733ff86c96f8acaf41cdc2cc98bf86fab31232c1
evelynmitchell <efmphone@gmail.com>
2024-02-26 11:00:06 -0700

    lint, gitigmore,  comment in the data loader to clear out the defn of the salesforce data

--- tsmixer/data_load.py
+++ tsmixer/data_load.py
@@ -3,13 +3,13 @@ from datasets import load_dataset
 dataset = load_dataset('Salesforce/cloudops_tsf', 'azure_vm_traces_2017')
 print(dataset)
 
-DatasetDict({
-    train_test: Dataset({
-        features: ['start', 'target', 'item_id', 'feat_static_cat', 'feat_static_real', 'past_feat_dynamic_real'],
-        num_rows: 17568
-    }),
-    pretrain: Dataset({
-        features: ['start', 'target', 'item_id', 'feat_static_cat', 'feat_static_real', 'past_feat_dynamic_real'],
-        num_rows: 159472
-    })
-})
+# DatasetDict({
+#     train_test: Dataset({
+#         features: ['start', 'target', 'item_id', 'feat_static_cat', 'feat_static_real', 'past_feat_dynamic_real'],
+#         num_rows: 17568
+#     }),
+#     pretrain: Dataset({
+#         features: ['start', 'target', 'item_id', 'feat_static_cat', 'feat_static_real', 'past_feat_dynamic_real'],
+#         num_rows: 159472
+#     })
+# })
