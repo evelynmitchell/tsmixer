c8ef5c225bf16595e114a44d04ad41bb3b6fb44e
evelynmitchell <efmphone@gmail.com>
2024-11-19 16:39:24 -0700

    linting name collision

--- scripts/data_split.py
+++ scripts/data_split.py
@@ -21,11 +21,11 @@ from tsmixer.data_load import load_data
 def split_data(data):
     """Split the data into train, test, and validation sets."""
     last_1000 = data.tail(1000)
-    train = last_1000.head(800)
-    test = last_1000[800:900]
-    validation = last_1000[900:1000]
+    tr = last_1000.head(800)
+    te = last_1000[800:900]
+    val = last_1000[900:1000]
 
-    return train, test, validation
+    return tr, te, val
 
 
 def save_to_csv(df, filename):
