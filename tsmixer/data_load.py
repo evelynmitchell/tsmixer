ffbd1cd615e12e7fc746b842bac6f7e01cff3803
evelynmitchell <efmphone@gmail.com>
2024-11-19 19:20:21 -0700

    choose a more appropriate dataset

--- tsmixer/data_load.py
+++ tsmixer/data_load.py
@@ -1,15 +1,7 @@
 from datasets import load_dataset
 
-dataset = load_dataset('Salesforce/cloudops_tsf', 'azure_vm_traces_2017')
-print(dataset)
 
-# DatasetDict({
-#     train_test: Dataset({
-#         features: ['start', 'target', 'item_id', 'feat_static_cat', 'feat_static_real', 'past_feat_dynamic_real'],
-#         num_rows: 17568
-#     }),
-#     pretrain: Dataset({
-#         features: ['start', 'target', 'item_id', 'feat_static_cat', 'feat_static_real', 'past_feat_dynamic_real'],
-#         num_rows: 159472
-#     })
-# })
+def load_data(dataset_name="mik3ml/timeseries-finance-ETF"):
+    """Load the dataset"""
+    ds = load_dataset(dataset_name)
+    return ds
