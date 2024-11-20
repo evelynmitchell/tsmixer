178726d20355cf69a62024195d359cf24ab72d87
evelynmitchell <efmphone@gmail.com>
2024-11-20 14:09:34 -0700

    linting, dataprep

--- tests/data/fin_preprocess/prep.py
+++ tests/data/fin_preprocess/prep.py
@@ -1,3 +1,4 @@
+""" Preprocess the financial data for the tsmixer model """
 # in ../train.csv we have a yahoo finance data set with the columns:
 # Date,Open,High,Low,Close,Adj Close,Volume
 
@@ -12,41 +13,17 @@
 # 3. calculate the log returns
 # 4. save the data to disk
 
-# I will use the following libraries:
-import pandas as pd
-import numpy as np
+from tsmixer.data_load import load_data
 
-# 1. read the data from disk
-data = pd.read_csv("../train.csv")
+# 1. read the data from hf
+data = load_data()
+
+print(data.head())
 
 # 2. calculate the detrended price
-data["detrended_price"] = data["Close"] - data["Close"].shift(1)
-data["detrended_high"] = data["High"] - data["High"].shift(1)
-data["detrended_low"] = data["Low"] - data["Low"].shift(1)
-data["detrended_open"] = data["Open"] - data["Open"].shift(1)
-data["detrended_adj_close"] = data["Adj Close"] - data[
-    "Adj Close"
-].shift(1)
+
 
 # 3. calculate the log returns
-data["log_returns"] = np.log(data["Close"]) - np.log(
-    data["Close"].shift(1)
-)
-data["log_returns_high"] = np.log(data["High"]) - np.log(
-    data["High"].shift(1)
-)
-data["log_returns_low"] = np.log(data["Low"]) - np.log(
-    data["Low"].shift(1)
-)
-data["log_returns_open"] = np.log(data["Open"]) - np.log(
-    data["Open"].shift(1)
-)
-data["log_returns_adj_close"] = np.log(data["Adj Close"]) - np.log(
-    data["Adj Close"].shift(1)
-)
-
-data["date"] = pd.to_datetime(data["Date"])
-data["volume"] = data["Volume"]
 
 
 # 4. save the data to disk
